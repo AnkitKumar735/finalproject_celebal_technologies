4bd5aa209adf14535b31e27c3d3b3ee49d35f82a
AnkitKumar735 <104652645+AnkitKumar735@users.noreply.github.com>
2024-08-03 06:09:40 +0530

    Add files via upload

---
+++ azure-container-deployment/scripts/deploy-aci.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Variables
+RESOURCE_GROUP="yourResourceGroup"
+ACI_NAME="yourACIName"
+ACR_NAME="youracrname"
+IMAGE_NAME="azure-container-app:$(Build.BuildId)"
+LOCATION="eastus"
+CPU=1
+MEMORY=1
+
+# Log in to Azure
+az login --service-principal -u $SP_APPID -p $SP_PASSWORD --tenant $TENANT_ID
+
+# Create or update the ACI instance
+az container create \
+  --resource-group $RESOURCE_GROUP \
+  --name $ACI_NAME \
+  --image $ACR_NAME.azurecr.io/$IMAGE_NAME \
+  --cpu $CPU \
+  --memory $MEMORY \
+  --registry-login-server $ACR_NAME.azurecr.io \
+  --registry-username $SP_APPID \
+  --registry-password $SP_PASSWORD \
+  --location $LOCATION \
+  --dns-name-label my-app-$RANDOM
